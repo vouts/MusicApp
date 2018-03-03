1fd80d647c93e4a99bc9895c3e503a5cf28e9611
Vouts <thevouts@gmail.com>
2018-03-03 13:02:50 +0200

    v1.0

---
+++ app/src/main/java/com/example/android/musicapp/Songs.java
@@ -0,0 +1,84 @@
+package com.example.android.musicapp;
+
+/**
+ * Created by Stelios on 3/3/2018.
+ */
+
+public class Songs {
+
+    /**
+     * Song name
+     */
+    private String mSongName;
+
+    /**
+     * Song artist
+     */
+    private String mSongArtist;
+
+    /**
+     * Song album name
+     */
+    private String mAlbumName;
+
+    /**
+     * Album image
+     */
+    private int mAlbumImage;
+
+    /**
+     * Song list image
+     */
+    private int mListImage = R.drawable.track;
+
+    /**
+     * Create a new Word object.
+     *
+     * @param songName is the title of the song
+     * @param songArtist is the artist of the song
+     * @param albumName is the title of the album
+     * @param albumImage is the cover of the album
+     */
+    public Songs(String songName, String songArtist, String albumName, int albumImage) {
+        mSongName = songName;
+        mSongArtist = songArtist;
+        mAlbumName = albumName;
+        mAlbumImage = albumImage;
+    }
+
+    /**
+     * Get the song title.
+     */
+    public String getSongName() {
+        return mSongName;
+    }
+
+    /**
+     * Get the song artist.
+     */
+    public String getSongArtist() {
+        return mSongArtist;
+    }
+
+    /**
+     * Get the album title.
+     */
+    public String getalbumName() {
+        return mAlbumName;
+    }
+
+    /**
+     * Get the album cover.
+     */
+    public int getAlbumImage() {
+        return mAlbumImage;
+    }
+
+    /**
+     * Get the image for the song list.
+     */
+    public int getListImage() {
+        return mListImage;
+    }
+
+}
