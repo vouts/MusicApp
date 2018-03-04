b72f68307e966a62d615775ca821ed1f8f5207d8
Vouts <thevouts@gmail.com>
2018-03-04 16:31:36 +0200

    v1.0

--- app/src/main/java/com/example/android/musicapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicapp/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.android.musicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +18,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ArrayList <Songs> songLit = new ArrayList<>();
+
+        songLit.add(new Songs("Song Name 1", "Artist 1", "Album name 1", R.drawable.album));
+        songLit.add(new Songs("Song Name 2", "Artist 2", "Album name 2", R.drawable.album));
+        songLit.add(new Songs("Song Name 3", "Artist 3", "Album name 3", R.drawable.album));
+        songLit.add(new Songs("Song Name 4", "Artist 4", "Album name 4", R.drawable.album));
+        songLit.add(new Songs("Song Name 5", "Artist 5", "Album name 5", R.drawable.album));
+        songLit.add(new Songs("Song Name 6", "Artist 6", "Album name 6", R.drawable.album));
+        songLit.add(new Songs("Song Name 7", "Artist 7", "Album name 7", R.drawable.album));
+        songLit.add(new Songs("Song Name 8", "Artist 8", "Album name 8", R.drawable.album));
+        songLit.add(new Songs("Song Name 9", "Artist 9", "Album name 9", R.drawable.album));
+        songLit.add(new Songs("Song Name 10", "Artist 10", "Album name 10", R.drawable.album));
+        songLit.add(new Songs("Song Name 11", "Artist 11", "Album name 11", R.drawable.album));
+        songLit.add(new Songs("Song Name 12", "Artist 12", "Album name 12", R.drawable.album));
+        songLit.add(new Songs("Song Name 13", "Artist 13", "Album name 13", R.drawable.album));
+        songLit.add(new Songs("Song Name 14", "Artist 14", "Album name 14", R.drawable.album));
+        songLit.add(new Songs("Song Name 15", "Artist 15", "Album name 15", R.drawable.album));
+        songLit.add(new Songs("Song Name 16", "Artist 16", "Album name 16", R.drawable.album));
+
+        SongsAdapter itemsAdapter = new SongsAdapter(this, songLit);
+
+        ListView listView = (ListView) findViewById(R.id.song_List);
+
+        listView.setAdapter(itemsAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                            @Override
+                                            public void onItemClick (AdapterView <?> adapter, View view,int position, long arg){
+                                                // TODO Auto-generated method stub
+                                                Intent ColorsIntent = new Intent(MainActivity.this, Splash.class);
+                                                startActivity(ColorsIntent);
+                                            }
+                                        }
+        );
 
     }
 }
---
+++ app/src/main/java/com/example/android/musicapp/SongsAdapter.java
@@ -0,0 +1,79 @@
+package com.example.android.musicapp;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Stelios on 3/3/2018.
+ */
+
+public class SongsAdapter extends ArrayAdapter<Songs> {
+    private static final String LOG_TAG = SongsAdapter.class.getSimpleName();
+
+    /**
+     * This is our own custom constructor (it doesn't mirror a superclass constructor).
+     * The context is used to inflate the layout file, and the list is the data we want
+     * to populate into the lists.
+     *
+     * @param context        The current context. Used to inflate the layout file.
+     * @param songList A List of AndroidFlavor objects to display in a list
+     */
+    public SongsAdapter(Activity context, ArrayList<Songs> songList) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // Because this is a custom adapter for three TextViews and an ImageView, the adapter is not
+        // going to use this second argument, so it can be any value. Here, we used 0.
+        super(context, 0, songList);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        Songs currentSong = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID listImage
+        ImageView listImage = (ImageView) listItemView.findViewById(R.id.list_Image);
+        // Get the list Image of the current object and
+        // set this image on the List image ImageView
+        listImage.setImageResource(currentSong.getListImage());
+
+        // Find the TextView in the list_item.xml layout with the ID song_title
+        TextView songName = (TextView) listItemView.findViewById(R.id.song_title);
+        // Get the song name of the current  object and
+        // set this text on the name TextView
+        songName.setText(currentSong.getSongName());
+
+        // Find the TextView in the list_item.xml layout with the ID song_artist
+        TextView songArtist = (TextView) listItemView.findViewById(R.id.song_artist);
+        // Get the Artist from the current Songs object and
+        // set this text on the Artist TextView
+        songArtist.setText(currentSong.getSongArtist());
+
+        // Find the TextView in the list_item.xml layout with the ID song_album
+        TextView songAlbum = (TextView) listItemView.findViewById(R.id.song_album);
+        // Get the Album title from the current Songs object and
+        // set this text on the Album TextView
+        songAlbum.setText(currentSong.getalbumName());
+
+
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
+        return listItemView;
+    }
+}
